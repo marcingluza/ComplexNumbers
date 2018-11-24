b662a9e2431f1077d709278e607ad1eb11601c4c
Marcin Gluza <gumball300@gmail.com>
2018-11-24 10:38:20 +0100

    base complex operations

--- ComplexNumbers/complex.cs
+++ ComplexNumbers/complex.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace ComplexNumbers
 {
-    class complex
+    class Complex
     {
+        public double R { get; set; }
+        public double I { get; set; }
+
+        private Complex(double _R, double _I)
+        {
+            R = _R;
+            I = _I;
+        }
+
+        public static Complex operator +(Complex num1, Complex num2)
+        {
+            return new Complex(num1.R + num2.R, num2.I + num2.I);
+        }
+
+        public static Complex operator -(Complex num1, Complex num2)
+        {
+            return new Complex(num1.R - num2.R, num2.I - num2.I);
+        }
+
+        public static Complex operator *(Complex num1, Complex num2)
+        {
+            return new Complex(
+            num1.R * num2.R - num1.I * num2.I,
+            num1.R * num2.I + num1.I * num2.R);
+        }
+
+        public static Complex operator /(Complex num1, Complex num2)
+        {
+            Complex con = new Complex(num1.R, -num2.I);
+            num2 *= con;
+
+            Complex numerator = num1 * con;
+
+            return new Complex(
+                numerator.R / num2.R,
+                numerator.I / num2.R);
+        }
+
+        public static Complex con(Complex num1)
+        {
+            return new Complex(num1.R, -num1.I);
+        }
+
+        public static Complex mod(Complex num1)
+        {
+            return new Complex(num1.R, -num1.I);
+        }
+
+
     }
 }

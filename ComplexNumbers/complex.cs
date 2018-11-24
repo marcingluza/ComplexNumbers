48dc808388fdc05510e0bf6cf3d8d567a6fb1eba
Marcin Gluza <gumball300@gmail.com>
2018-11-24 11:19:28 +0100

    added vectors

--- ComplexNumbers/complex.cs
+++ ComplexNumbers/complex.cs
@@ -8,10 +8,10 @@ namespace ComplexNumbers
 {
     class Complex
     {
-        public double R { get; set; }
-        public double I { get; set; }
+        public int R { get; set; }
+        public int I { get; set; }
 
-        private Complex(double _R, double _I)
+        public Complex(int _R, int _I)
         {
             R = _R;
             I = _I;
@@ -51,11 +51,67 @@ namespace ComplexNumbers
             return new Complex(num1.R, -num1.I);
         }
 
-        public static Complex mod(Complex num1)
+        public static double mod(Complex num1)
         {
-            return new Complex(num1.R, -num1.I);
+            double _mod = Math.Sqrt(Math.Pow(num1.R, 2) + Math.Pow(num1.I, 2));
+            return _mod;
         }
 
 
+    }
+
+    class ComplexV
+    {
+        public Complex[] vector;
+
+        private ComplexV(Complex[] W)
+        {
+            this.vector = W;
+        }
+
+        public static Complex[] AddVectors (ComplexV W, ComplexV V)
+        {
+            Complex[] add = new Complex[W.vector.Length];
+            for(int i=0; i<W.vector.Length; i++)
+            {
+               // add[i] = new Complex(W.vector[i].R + V.vector[i].R, W.vector[i].I + V.vector[i].I);
+                add[i] = W.vector[i] + V.vector[i];
+            }
+
+            return add;
+        }
+
+        public static Complex[] SubVectors(ComplexV W, ComplexV V)
+        {
+            Complex[] sub = new Complex[W.vector.Length];
+            for (int i = 0; i < W.vector.Length; i++)
+            {
+                sub[i] = W.vector[i] - V.vector[i];
+            }
+
+            return sub;
+        }
+
+        public static Complex[] Multip(ComplexV W, int a)
+        {
+            Complex[] multip = new Complex[W.vector.Length];
+            for (int i = 0; i < W.vector.Length; i++)
+            {
+                multip[i] = new Complex(W.vector[i].R * a, W.vector[i].I * a);
+            }
+
+            return multip;
+        }
+
+        public static Complex[] conV(ComplexV W, ComplexV V)
+        {
+            Complex[] multip = new Complex[W.vector.Length];
+            for (int i = 0; i < W.vector.Length; i++)
+            {
+                multip[i] = W.vector[i] * Complex.con(V.vector[i]);
+            }
+
+            return multip;
+        }
     }
 }
